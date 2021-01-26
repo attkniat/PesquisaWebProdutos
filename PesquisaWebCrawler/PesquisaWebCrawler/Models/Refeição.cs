ff0373d1482b7f945a732177a18a4b409a496099
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2021-01-26 18:56:38 -0300

    Model Refefeição | Inclusão
    Inclusão de modelo chamado Refeição

---
+++ PesquisaWebCrawler/PesquisaWebCrawler/Models/Refeição.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PesquisaWebCrawler.Models
+{
+    public class Refeição
+    {
+        public string nomeProduto { get; set; }
+        public string descricaoProduto { get; set; }
+        public string imagemProduto { get; set; }
+        public string linkProduto { get; set; }
+    }
+}
