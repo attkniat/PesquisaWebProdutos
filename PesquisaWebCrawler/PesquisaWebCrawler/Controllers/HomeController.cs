6f5d3ce278a1e9ebca2b8b2cabd7fabb7d862ead
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2021-01-26 18:51:26 -0300

    Crawler | Implentação
    Crawler para retornar dados de um determinado produto.

--- PesquisaWebCrawler/PesquisaWebCrawler/Controllers/HomeController.cs
+++ PesquisaWebCrawler/PesquisaWebCrawler/Controllers/HomeController.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using PesquisaWebCrawler.Models;
+using HtmlAgilityPack;
 
 namespace PesquisaWebCrawler.Controllers
 {
     public class HomeController : Controller
     {
         #region logger
-        
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -22,8 +24,31 @@ namespace PesquisaWebCrawler.Controllers
 
         #endregion
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(string produtoPesquisado)
         {
+            if (produtoPesquisado != null)
+            {
+                using (var httpCliente = new HttpClient())
+                {
+                    var url = "https://www.giraffas.com.br/?s=" + produtoPesquisado;
+                    var htmlContent = await httpCliente.GetStringAsync(url);
+
+                    var htmlDocument = new HtmlDocument();
+                    htmlDocument.LoadHtml(htmlContent);
+
+                    var divsProdutos = htmlDocument.DocumentNode.Descendants("div")
+                        .Where(node => node.GetAttributeValue("class", "")
+                        .Equals("c-highlight-box")).ToList();
+
+                    foreach (var produtos in divsProdutos)
+                    {
+                        var nomeProduto = produtos?.Descendants("h2")?.FirstOrDefault().InnerText;
+                        var descricaoProduto = produtos?.Descendants("p")?.FirstOrDefault().InnerText;
+                        var imagemProduto = produtos?.Descendants("img")?.FirstOrDefault().ChildAttributes("src").FirstOrDefault().Value;
+                        var linkProduto = produtos?.Descendants("a")?.FirstOrDefault().ChildAttributes("href").FirstOrDefault().Value;
+                    }
+                }
+            }
             return View();
         }
     }
