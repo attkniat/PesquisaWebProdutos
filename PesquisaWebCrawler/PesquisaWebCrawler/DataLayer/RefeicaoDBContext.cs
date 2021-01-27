5a8dfd91c05c203fe7bc5a3ffc181e0c7eb84991
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2021-01-27 11:07:46 -0300

    DBContext | Criação
    Criação de contexto para model

---
+++ PesquisaWebCrawler/PesquisaWebCrawler/DataLayer/RefeicaoDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PesquisaWebCrawler.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PesquisaWebCrawler.DataLayer
+{
+    public class RefeicaoDBContext : DbContext
+    {
+        public RefeicaoDBContext(DbContextOptions<RefeicaoDBContext> options) : base(options) { }
+        public DbSet<Refeição> Refeicao { get; set; }
+    }
+}
