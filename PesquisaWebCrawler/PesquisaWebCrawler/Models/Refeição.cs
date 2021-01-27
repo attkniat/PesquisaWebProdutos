df94526ff58bae0f8058f71a26cb4d4e3eb161cd
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2021-01-27 11:02:58 -0300

    Model Refeição | Update
    Preparando Model para Migration

--- PesquisaWebCrawler/PesquisaWebCrawler/Models/Refeição.cs
+++ PesquisaWebCrawler/PesquisaWebCrawler/Models/Refeição.cs
@@ -1,6 +1,8 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +10,21 @@ namespace PesquisaWebCrawler.Models
 {
     public class Refeição
     {
+        [Key]
+        [Column("PROCODIGO")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string codigoProduto { get; set; }
+
+        [Column("PRONOME")]
         public string nomeProduto { get; set; }
+
+        [Column("PRODESCRICAO")]
         public string descricaoProduto { get; set; }
+
+        [Column("PROIMAGEM")]
         public string imagemProduto { get; set; }
+
+        [Column("PROLINK")]
         public string linkProduto { get; set; }
     }
 }
